46dbd9d045a46c397f6d39a377ec75e25e2797ff
Robbie Porter <rthomporter@gmail.com>
2019-11-14 16:54:55 -0600

    MVP + Stretch

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import FormComp from "./Form";
+
+function App() {
+  return (
+    <div className="App">
+      <FormComp />
+    </div>
+  );
+}
+
+export default App;
